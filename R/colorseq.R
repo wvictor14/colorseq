881000a6f8802f3d03d7074c4b91975478769d0c
Victor Yuan <victor.2wy@gmail.com>
2024-03-29 15:20:23 -0700

    initial colorseq function

--- R/colorseq.R
+++ R/colorseq.R
@@ -1,10 +1,3 @@
-
-#'
-#'
-# on 1 string
-one_seq <- aa_seq$seq[1]
-.color_sequence(one_seq)
-
 #' Applies html coloring to biological sequences
 #'
 #' Takes a vector of biological sequences and applies html coloring. Coloring
@@ -17,7 +10,7 @@ one_seq <- aa_seq$seq[1]
 #'
 #' - "clustal": https://www.bioinformatics.nl/~berndb/aacolour.html
 #'
-#' @return
+#' @return vector of html colored sequences
 #' @export
 #'
 #' @examples
